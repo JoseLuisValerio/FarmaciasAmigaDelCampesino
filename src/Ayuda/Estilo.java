29ca6ad1e008cac03ad562dbc4eb45aa2901c38a
4lejandroRojas <4lejandroRojas@gmail.com>
2016-07-16 15:22:57 -0500

    Farmacia Java
    Proyecto Java

---
+++ src/Ayuda/Estilo.java
@@ -0,0 +1,20 @@
+/**
+ * Contiene clases necesarias para facilitar configuraciones globales
+ */
+package Ayuda;
+import java.awt.Font;
+import javax.swing.JLabel;
+/**
+ * Da formato visual a los elementos de las vistas
+ */
+public class Estilo {
+    
+    public static void lblTitulo(JLabel label){
+        label.setFont(new java.awt.Font("Science Fair",Font.PLAIN,14));
+        
+    }
+    
+    public static void lblBody(JLabel label){
+        
+    }
+}
