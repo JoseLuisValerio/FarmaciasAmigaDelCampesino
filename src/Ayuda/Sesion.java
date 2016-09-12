e3d1ece7404ba29726c439b202f0ed9961e0e6bc
JoseLuisValerio <ariesjl_92@hotmail.com>
2016-09-12 14:50:24 -0500

    Errores de Sesión

--- src/Ayuda/Sesion.java
+++ src/Ayuda/Sesion.java
@@ -14,83 +14,86 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 /**
  *
- * @author Jose Luis
- Clase que Recibe LeerSesion para la creación de sesiones
+ * @author Jose Luis Clase que Recibe LeerSesion para la creación de sesiones
  */
 public class Sesion {
-   static ArrayList<String>datosSesion= new ArrayList<String>();
-   static ArrayList<Integer>permisosSesion= new ArrayList<Integer>();
 
-   
-    public static void iniciarSesion(String user,String name,String tuser, String idUsuario) throws IOException{
+    static ArrayList<String> datosSesion = null;
+    static ArrayList<Integer> permisosSesion = null;
+
+
+    public static void iniciarSesion(String user, String name, String tuser, String idUsuario) throws IOException {
 //rebie el usuario , el nombre de usuario  y el tipo para asignar permisos
+        datosSesion = new ArrayList<String>();
+        permisosSesion = new ArrayList<Integer>();
         datosSesion.add(user);
         datosSesion.add(name);
         datosSesion.add(tuser);
         datosSesion.add(idUsuario);
-        
-		/** FORMA 2 DE ESCRITURA. Con el fichero codificado en UTF-8 **/
-		Writer out = null;
-		try {
-			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("datosSesion.frm"), "UTF-8"));
-			
-			// Escribimos linea a linea en el fichero
-			for (String linea :datosSesion) {
-				try {
-					out.write(linea+"\n");
-				} catch (IOException ex) {
-					System.err.println("Mensaje excepcion escritura: " + ex.getMessage());
-				}
-			}
 
-		} catch (UnsupportedEncodingException | FileNotFoundException ex2) {
-			System.err.println("Mensaje error 2: " + ex2.getMessage());
-		} finally {
-			try {
-				out.close();
-			} catch (IOException ex3) {
-				System.err.println("Mensaje error cierre fichero: " + ex3.getMessage());
-			}
-		}
-        
-        
+        /**
+         * FORMA 2 DE ESCRITURA. Con el fichero codificado en UTF-8 *
+         */
+        Writer out = null;
+        try {
+            out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("datosSesion.frm"), "UTF-8"));
+
+            // Escribimos linea a linea en el fichero
+            for (String linea : datosSesion) {
+                try {
+                    out.write(linea + "\n");
+                } catch (IOException ex) {
+                    System.err.println("Mensaje excepcion escritura: " + ex.getMessage());
+                }
+            }
+
+        } catch (UnsupportedEncodingException | FileNotFoundException ex2) {
+            System.err.println("Mensaje error 2: " + ex2.getMessage());
+        } finally {
+            try {
+                out.close();
+            } catch (IOException ex3) {
+                System.err.println("Mensaje error cierre fichero: " + ex3.getMessage());
+            }
+        }
+
     }
-    
-    public static void cerrarSesion(){
+
+    public static void cerrarSesion() {
         //Identifica el archivo creado para eliminar los daots creados en la sesion
-    File fichero = new File("datosSesion.frm");
-    if (fichero.delete()){
-   System.err.println("El fichero ha sido borrado satisfactoriamente");
-    }else{
-   System.err.println("El fichero no puede ser borrado");
-    }    
+        File fichero = new File("datosSesion.frm");
+        if (fichero.delete()) {
+            System.err.println("El fichero ha sido borrado satisfactoriamente");
+        } else {
+            System.err.println("El fichero no puede ser borrado");
+        }
     }
-    public static ArrayList<String> obtenerSesion() throws IOException{
-       //Retorna los LeerSesion de la sesion en en un ArrayList de tipo String con los LeerSesion leidos del archivo de la sesión
+
+    public static ArrayList<String> obtenerSesion() throws IOException {
+        //Retorna los LeerSesion de la sesion en en un ArrayList de tipo String con los LeerSesion leidos del archivo de la sesión
         String cadena;
-        ArrayList <String> datos= new ArrayList <String>();
-      FileReader f = new FileReader("datosSesion.frm");
-      BufferedReader b = new BufferedReader(f);
-      while((cadena = b.readLine())!=null) {
-          datos.add(cadena);
-      }
-      b.close();
-      
-       return datos;
+        ArrayList<String> datos = new ArrayList<String>();
+        FileReader f = new FileReader("datosSesion.frm");
+        BufferedReader b = new BufferedReader(f);
+        while ((cadena = b.readLine()) != null) {
+            datos.add(cadena);
+        }
+        b.close();
+
+        return datos;
     }
-    
-    public static String  LeerSesion(String opc){
+
+    public static String LeerSesion(String opc) {
         //Carga los datoso Obtenido de la clase se decion mas para ser mostradosen las etiquetas
-    ArrayList <String> Permisos = new ArrayList <String>();
-       try {
-           Permisos=obtenerSesion();
-       } catch (IOException ex) {
-           Logger.getLogger(Sesion.class.getName()).log(Level.SEVERE, null, ex);
-       }
-        switch(opc){
+        ArrayList<String> Permisos = new ArrayList<String>();
+        try {
+            Permisos = obtenerSesion();
+        } catch (IOException ex) {
+            Logger.getLogger(Sesion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        switch (opc) {
             case "Usuario":
                 return Permisos.get(0);
             case "Nombre":
@@ -102,6 +105,6 @@ public class Sesion {
             default:
                 return "No definido";
         }
-        
+
     }
 }
